92d028c404c38ca9836b6d14ce97e2f015297205
Laia <190077@iessanalberto.com>
2024-01-19 12:45:26 +0100

    Commit Laia 19/01

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import RecuperarContrasena from "./pantallas/recuperarContrasena";
+import styles from './estilos/estilos'
+import { View } from "react-native";
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Hola mundo segunda fase</Text>
-      <StatusBar style="auto" />
+    <View style = {styles.container}>
+      <RecuperarContrasena />
     </View>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     flexDirection: "column",
+//     alignItems: "center",
+//     justifyContent: "space-around",
+//     backgroundColor: "lavender",
+//     ...Platform.select({
+//       ios: { paddingTop: 20 },
+//       android: { paddingTop: StatusBar.currentHeight }
+//     })
+//   },
+
+//   box: {
+//     width: 300,
+//     height: 100,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     backgroundColor: "white",
+//     borderWidth: 1,
+//     borderStyle: "dashed",
+//     borderColor: "pink"
+//   },
+//   boxText: {
+//     color: "violet",
+//     fontWeight: "bold"
+//   }
+// });
---
+++ pantallas/recuperarContrasena.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Button, View, TextInput, TouchableOpacity, Alert, Text } from "react-native";
+import Icon from 'react-native-vector-icons/Ionicons';
+import styles from '../estilos/estilos'
+const RecuperarContrasena = () => {
+    const [text, onChangeText] = React.useState('');
+
+    const handleBackPress = () => {
+        // Lógica para manejar el botón de flecha hacia atrás
+        // Puedes utilizar la navegación, por ejemplo, si estás en una pantalla de navegación.
+
+        Alert.alert('Se ha pulsado el botón FLECHA');
+        
+
+    };
+
+
+    return (
+
+        // TODO boton flotante navegación <--
+
+        <View>
+
+            <TouchableOpacity onPress={handleBackPress} style={styles.backButton}>
+                <Icon name="arrow-back" size={30} color="#ec5f28" />
+            </TouchableOpacity>
+
+
+            <Text style={styles.titles}>
+                Restablecer la contraseña
+            </Text>
+
+            <Text style={styles.text}>
+                Por favor, introduzca su dirección de correo. Le enviaremos las instrucciones para restablecer su contraseña.
+            </Text>
+
+            <TextInput
+                style={styles.inputs}
+                onChangeText={onChangeText}
+                value={text}
+            />
+
+            <Button
+                color='#ec5f28'
+                title="Recuperar contraseña"
+                onPress={() => {
+                    Alert.alert('Se ha enviado un correo a la siguiente dirección:', `${text}`);
+                }}
+            />
+        </View>
+
+    );
+};
+
+export default RecuperarContrasena;
