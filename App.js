4fe966df3e478430dcb68c909251b90a38c6c34e
Miguel <miguel891906@gmail.com>
2024-01-23 12:54:08 +0100

    commit

--- App.js
+++ App.js
@@ -4,9 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from "./Ventanas/Login";
 import Register from './Ventanas/Register';
 import RecuperarContrasena from "./pantallas/recuperarContrasena";
-import styles from './estilos/estilos'
-import { View } from "react-native";
-import styles from './estilos/estilos'
+
 
 const Stack = createNativeStackNavigator();
 
@@ -25,44 +23,3 @@ function App(){
   );
 }
 export default App;
-
-
-
-// export default function App() {
-
-//   return (
-//     <View style = {styles.container}>
-//       <RecuperarContrasena />
-//     </View>
-
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     flexDirection: "column",
-//     alignItems: "center",
-//     justifyContent: "space-around",
-//     backgroundColor: "lavender",
-//     ...Platform.select({
-//       ios: { paddingTop: 20 },
-//       android: { paddingTop: StatusBar.currentHeight }
-//     })
-//   },
-
-//   box: {
-//     width: 300,
-//     height: 100,
-//     justifyContent: "center",
-//     alignItems: "center",
-//     backgroundColor: "white",
-//     borderWidth: 1,
-//     borderStyle: "dashed",
-//     borderColor: "pink"
-//   },
-//   boxText: {
-//     color: "violet",
-//     fontWeight: "bold"
-//   }
-// });
